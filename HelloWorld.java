6a66fb89360a176ef10c32b5f4c093428c34e908
PAWAN <pawan4utit@gmail.com>
2017-06-17 10:07:54 +0530

    modified chnages

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,8 @@ public class HelloWorld{
 
   public static void main(String args[]){
 
-    for(int i=1; i<=10; i++)
+    for(int i=1; i<=9; i++)
 
-       System.out.println("Hello World...."+i);
+       System.out.println("HelloWorld...."+i);
    }
 }
