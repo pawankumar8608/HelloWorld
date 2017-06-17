da53e73daff5de7e5ac41ef392f168f30e3613e0
PAWAN <pawan4utit@gmail.com>
2017-06-17 10:53:42 +0530

    modified changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,12 +2,8 @@ public class HelloWorld{
 
   public static void main(String args[]){
 
-<<<<<<< HEAD
     for(int i=1; i<=12; i++)
-=======
-    for(int i=1; i<=10; i++)
->>>>>>> origin/master
-
+		
        System.out.println("Hello World...."+i);
    }
 }
