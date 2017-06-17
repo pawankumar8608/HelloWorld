08eea21b1149fc1943af9d736acc9cbaf2722146
Pawan Kumar <pawan4utit@gmail.com>
2017-06-17 10:16:04 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld{
 
   public static void main(String args[]){
 
-    for(int i=1; i<=9; i++)
+    for(int i=1; i<=10; i++)
 
        System.out.println("HelloWorld...."+i);
    }
